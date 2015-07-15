bebaab825030ddc452607c4e6afb600c0ade73c4
Steven Berlanga <zabawaba99@gmail.com>
2015-07-15 17:21:42 -0400

    updating server tests

--- server_test.go
+++ server_test.go
@@ -94,40 +94,13 @@ func TestSet(t *testing.T) {
 	req, err := http.NewRequest("PUT", ft.URL+"/foo.json", strings.NewReader(body))
 	require.NoError(t, err)
 	resp := httptest.NewRecorder()
-	ft.set(resp, req)
+	ft.ServeHTTP(resp, req)
 
 	// ASSERT
 	assert.Equal(t, http.StatusOK, resp.Code)
 	respBody, err := ioutil.ReadAll(resp.Body)
 	require.NoError(t, err)
 	assert.Equal(t, body, string(respBody))
-}
-
-func TestPatch(t *testing.T) {
-	// ARRANGE
-	ft := New()
-	require.NoError(t, ft.Start())
-
-	path := "some/awesome/path"
-	body := map[string]interface{}{
-		"foo":  "bar",
-		"fooy": true,
-		"bar":  []interface{}{false, "lolz"},
-	}
-	ft.db.add(path, newNode(body))
-
-	// ACT
-	newVal := `"notbar"`
-	req, err := http.NewRequest("PATCH", ft.URL+"/some/awesome/path/foo.json", strings.NewReader(newVal))
-	require.NoError(t, err)
-	resp := httptest.NewRecorder()
-	ft.set(resp, req)
-
-	// ASSERT
-	assert.Equal(t, http.StatusOK, resp.Code)
-	respBody, err := ioutil.ReadAll(resp.Body)
-	require.NoError(t, err)
-	assert.Equal(t, newVal, string(respBody))
 }
 
 func TestSet_NoBody(t *testing.T) {
@@ -139,7 +112,7 @@ func TestSet_NoBody(t *testing.T) {
 	req, err := http.NewRequest("PUT", ft.URL+"/.json", bytes.NewReader(nil))
 	require.NoError(t, err)
 	resp := httptest.NewRecorder()
-	ft.set(resp, req)
+	ft.ServeHTTP(resp, req)
 
 	// ASSERT
 	assert.Equal(t, http.StatusBadRequest, resp.Code)
@@ -157,7 +130,91 @@ func TestSet_InvalidBody(t *testing.T) {
 	req, err := http.NewRequest("PUT", ft.URL+"/.json", strings.NewReader("{asd}"))
 	require.NoError(t, err)
 	resp := httptest.NewRecorder()
-	ft.set(resp, req)
+	ft.ServeHTTP(resp, req)
+
+	// ASSERT
+	assert.Equal(t, http.StatusBadRequest, resp.Code)
+	b, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+	assert.Equal(t, invalidJSON, string(b))
+}
+
+func TestDel(t *testing.T) {
+	// ARRANGE
+	ft := New()
+	require.NoError(t, ft.Start())
+	path := "foo/bar"
+	n := newNode(2)
+	ft.db.add(path, n)
+
+	// ACT
+	req, err := http.NewRequest("DELETE", ft.URL+"/"+path+".json", nil)
+	require.NoError(t, err)
+
+	resp := httptest.NewRecorder()
+	ft.ServeHTTP(resp, req)
+
+	// ASSERT
+	assert.Equal(t, http.StatusOK, resp.Code)
+	n = ft.db.get(path)
+	assert.Nil(t, n)
+}
+
+func TestUpdate(t *testing.T) {
+	// ARRANGE
+	ft := New()
+	require.NoError(t, ft.Start())
+
+	path := "some/awesome/path"
+	body := map[string]interface{}{
+		"foo":  "bar",
+		"fooy": true,
+		"bar":  []interface{}{false, "lolz"},
+	}
+	ft.db.add(path, newNode(body))
+
+	// ACT
+	newVal := `"notbar"`
+	req, err := http.NewRequest("PATCH", ft.URL+"/some/awesome/path/foo.json", strings.NewReader(newVal))
+	require.NoError(t, err)
+	resp := httptest.NewRecorder()
+	ft.ServeHTTP(resp, req)
+
+	// ASSERT
+	assert.Equal(t, http.StatusOK, resp.Code)
+	respBody, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+	assert.Equal(t, newVal, string(respBody))
+}
+
+func TestUpdate_NoBody(t *testing.T) {
+	// ARRANGE
+	ft := New()
+	require.NoError(t, ft.Start())
+
+	// ACT
+	req, err := http.NewRequest("PATCH", ft.URL+"/.json", bytes.NewReader(nil))
+	require.NoError(t, err)
+	resp := httptest.NewRecorder()
+	ft.ServeHTTP(resp, req)
+
+	// ASSERT
+	assert.Equal(t, http.StatusBadRequest, resp.Code)
+	b, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+	assert.Equal(t, missingBody, string(b))
+}
+
+func TestUpdate_InvalidBody(t *testing.T) {
+	// ARRANGE
+	ft := New()
+	require.NoError(t, ft.Start())
+
+	// ACT
+	req, err := http.NewRequest("PATCH", ft.URL+"/.json", strings.NewReader("{asd}"))
+	require.NoError(t, err)
+	resp := httptest.NewRecorder()
+	ft.ServeHTTP(resp, req)
 
 	// ASSERT
 	assert.Equal(t, http.StatusBadRequest, resp.Code)
@@ -186,7 +243,7 @@ func TestGet(t *testing.T) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%s.json", ft.URL, path), bytes.NewReader(b))
 	require.NoError(t, err)
 	resp := httptest.NewRecorder()
-	ft.get(resp, req)
+	ft.ServeHTTP(resp, req)
 
 	// ASSERT
 	assert.Equal(t, http.StatusOK, resp.Code)
